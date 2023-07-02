eef92f6986a2ccca69a6ff4594f219d4b4d8fb3f
Ian Deans <ideans715@gmail.com>
2023-07-02 12:34:37 -0700

    add stuff with K

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,36 @@
-use rand::seq::SliceRandom;
-use std::io::{self, Write};
+
+
+// fn greet(names: Vec<String>) {
+//     println!("Welcome, {:?} to the Rust world!", names);
+//     names.clear();
+// }
+// fn main() {
+//     let mut names = vec!["Rusty".to_string(), "Marcel".to_string()];
+//     greet(&mut names);
+//     names.push("John".to_string());
+//     greet(&mut names);
+// }
+
+
+// fn greet(mut names: Vec<String>) -> Vec<String> {
+//     println!("Welcome, {:?} to the Rust world!", names);
+//     names.clear();
+//     names.push("John".to_string());
+//     names
+// }
+// fn main() {
+//     let names = vec!["Rusty".to_string(), "Marconious".to_string()];
+//     let new_names = greet(names);
+//     greet(new_names);
+// }
+
+
+fn greet(names: Vec<String>) {
+    println!("Whaddup, {:?}.", names);
+}
 
 fn main() {
-    let words = vec!["apple", "banana", "cherry", "date", "elderberry"];
-    let mut rng = rand::thread_rng();
-    let chosen_word = words.choose(&mut rng).unwrap();
-    let mut guessed_letters: Vec<char> = vec!['-'; chosen_word.len()];
-    let mut all_guesses: Vec<char> = Vec::new();
-    let mut attempts = 6;
 
-
-    println!("Welcome to Hangman!");
-
-    loop {
-        println!("Word: {}", guessed_letters.iter().collect::<String>());
-        println!("Attempts Left: {}", attempts);
-        println!("Guessed letters: {}", all_guesses.iter().collect::<String>());
-
-        println!("Enter a letter: ");
-        io::stdout().flush().unwrap();
-
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).unwrap();
-        let guess = guess.trim().chars().next().unwrap();
-
-        let mut found = false;
-        for (i, letter) in chosen_word.chars().enumerate() {
-            all_guesses.push(letter);
-            if letter == guess {
-                guessed_letters[i] = letter;
-                found = true;
-            }
-        }
-
-        if !found {
-            attempts -= 1;
-        }
-
-        if attempts == 0 {
-            println!("Game over! The word was: {}", chosen_word);
-            break;
-        }
-
-        if guessed_letters.iter().all(|&c| c != '-') {
-            println!("Congratulations! You guessed the word: {}", chosen_word);
-            break;
-        }
-    }
+    let names = vec!["Kaleb".to_string(), "Ian".to_string()];
+    greet( names );
 }
