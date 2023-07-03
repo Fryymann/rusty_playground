622964840b152a411a96ed4e8c5a3b9a88915133
Ian Deans <ideans715@gmail.com>
2023-07-02 21:46:25 -0700

    get stuck

--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,80 @@
-use std::fmt;
-
-// fn greet(names: Vec<String>) {
-//     println!("Welcome, {:?} to the Rust world!", names);
-//     names.clear();
-// }
-// fn main() {
-//     let mut names = vec!["Rusty".to_string(), "Marcel".to_string()];
-//     greet(&mut names);
-//     names.push("John".to_string());
-//     greet(&mut names);
-// }
-
-
-// fn greet(mut names: Vec<String>) -> Vec<String> {
-//     println!("Welcome, {:?} to the Rust world!", names);
-//     names.clear();
-//     names.push("John".to_string());
-//     names
-// }
-// fn main() {
-//     let names = vec!["Rusty".to_string(), "Marconious".to_string()];
-//     let new_names = greet(names);
-//     greet(new_names);
-// }
-
-
-// fn greet(mut names: Vec<String>) -> Vec<String> {
-//     println!("Whaddup, {:?}.", names);
-//     names.clear();
-//     names
-// }
-
-// fn add_sol(mut names: Vec<String> ) -> Vec<String>{
-//     names.push("Sol".to_string());
-//     names
-// }
-
-// fn main() {
-//     let names = vec!["Kaleb".to_string(), "Ian".to_string()];
-//     let new_names = greet(names);
-//     let new_new_names = add_sol( new_names );
-//     greet( new_new_names );
-// }
-
-
-// struct Person {
-//     name: String,
-//     age: u32,
-// }
-
-// impl fmt::Display for Person {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         write!(f, "Name: {} | Age: {}", &self.name, &self.age)
-//     }
-// }
-
-// impl Person {
-//     fn age(&mut self) {
-//         self.age = &self.age + 1;
-//     }
-// }
-
-// fn age_person(person: &mut Person) {
-//     person.age = person.age + 1;
-// }
-
-// fn main() {
-//     let mut ian = Person{name: "Ian".to_string(), age: 36};
-//     let kaleb = Person{name: "Kaleb".to_string(), age: 19};
-
-//     println!("{}, {}", ian, kaleb);
-
-//     age_person( &mut ian );
-//     ian.age();
-
-//     println!("{}, {}", ian, kaleb);
-// }
 
 
 
-struct Person {
-    name: String,
-    age: u32
+/**
+ *
+ * Todo - struct that represents an individual task
+ *
+ * TodoList - struct that manages Todos.
+ *
+ *
+ */
+#[derive(Debug)]
+struct Todo {
+    description: String,
+    complete: bool,
 }
 
-impl Person {
-    fn greet(&self) {
-        println!("Hi, my name is {}.", &self.name);
+impl Todo {
+    fn complete_task(&mut self) {
+        self.complete = true;
     }
 
-    fn wave(&self) {
-        println!("{name} Waves", name = &self.name);
-    }
 
-    fn slap(&self, target: &Person) {
-        println!("{} rears back and slaps the shit out of {}!", &self.name, &target.name);
+    fn print(&self) {
+        println!("Description: {description} - Completed: {complete}", description = self.description, complete = self.complete);
     }
 }
 
+struct TodoList {
+    list: Vec<Todo>,
+}
 
-impl fmt::Display for Person {
-    fn fmt(&self, k: &mut fmt::Formatter) -> fmt::Result {
-        write!(k, "name {}, age {}", &self.name, &self.age)
+impl TodoList {
+
+    fn add_todo(&mut self, desc: String) {
+        let new_todo = Todo{ description: desc, complete: false };
+        self.list.push(new_todo);
+    }
+
+    fn print_list(&self) {
+        for todo in &self.list {
+            todo.print();
+        }
+    }
+
+
+    fn complete_task(&mut self, index: usize) {
+        self.list[index].complete_task();
+    }
+
+    fn clear_completed_todos(&mut self) {
+        self.list = self.list.iter()
+            .filter(|tasks| tasks.complete)
+            .collect();
     }
 }
 
 fn main(){
-    let kaleb =  Person{name: "Kaleb".to_string(),  age: 19};
-    let ian: Person = Person{name: "Ian".to_string(), age: 36};
 
-    println!("{}", kaleb);
+    let mut task_list = TodoList{ list: Vec::new() };
 
-    kaleb.greet();
-    kaleb.wave();
+    task_list.add_todo( "Water the dogs".to_string() );
+    task_list.add_todo( "Learn structs in Rust".to_string() );
 
-    kaleb.slap(&ian);
+    task_list.complete_task(1);
+    task_list.print_list();
+
+    task_list.clear_completed_todos();
+
+    task_list.print_list();
 }
+
+// let mut some_vec = vec![0, 10, 20, 30];
+// if let Some(index) = some_vec.iter().find(|value| **value == 10) {
+//     some_vec.swap_remove(*index);
+// }
+
+// if let Some(index) = some_vec.iter().position(|value| *value == 10) {
+//     some_vec.swap_remove(index);
+// }
